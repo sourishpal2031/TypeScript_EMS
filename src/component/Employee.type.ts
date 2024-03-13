b3e12fe692b69aa3d07117bfb7a075ff14e6c41a
sourishpal2031 <sourishpal2031@gmail.com>
2024-03-13 23:42:51 +0530

    Initial Commit

---
+++ src/component/Employee.type.ts
@@ -0,0 +1,19 @@
+export interface IEmployee{
+    id:string;
+    firstName: string;
+    lastName:string;
+    email:string
+}
+
+export const dummyEmployeeList:IEmployee[]=[{
+    id: new Date().toJSON().toString(),
+    firstName:"Dummy1",
+    lastName:"Dummy11",
+    email:"dummy1"
+},];
+
+export enum PageEnum{
+list,
+add,
+edit,
+}
---
+++ src/component/EmployeeModal.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./EmployeeModal.style.css"
+import { IEmployee } from './Employee.type';
+
+type Props={
+    onClose:()=>void;
+    data:IEmployee
+}
+
+const EmployeeModal = (props: Props) => {
+    const{onClose, data}=props
+  return (
+    <div id="myModal" className="modal">
+  <div className="modal-content">
+    <span className="close" onClick={onClose}>&times;</span>
+    <h3>Employee Data</h3>
+    <div>
+        <div>
+            <label>First Name: {data.firstName}</label>
+        </div>
+        <div>
+            <label>Last Name: {data.lastName}</label>
+        </div>
+        <div>
+            <label>Email Add: {data.email}</label>
+        </div>
+    </div>
+    
+  </div>
+
+</div>
+  )
+}
+
+export default EmployeeModal
